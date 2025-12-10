518f1f4f255b63fd1fd4cc3ac5c363a61449c009
Mangloo69 <yadav69.swayam@gmail.com>
2025-12-10 19:19:33 +0545

    Added Execution history with error and success output for users to track there workflows executions

--- src/inngest/utils.ts
+++ src/inngest/utils.ts
@@ -1,6 +1,7 @@
 import { Connection, Node } from "@/generated/prisma/client";
 import toposort from "toposort";
 import { inngest } from "./client";
+import { createId } from "@paralleldrive/cuid2";
 
 export const topologicalSort = (
   nodes: Node[],
@@ -55,5 +56,6 @@ export const sendWorkflowExecution = async (data: {
   await inngest.send({
     name: "workflows/execute.workflow",
     data,
+    id: createId(),
   });
 };
