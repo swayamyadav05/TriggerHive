c24d121a7fbd1713ad48fe6b6e062c6327ab3e06
Mangloo69 <yadav69.swayam@gmail.com>
2025-12-11 20:56:49 +0545

    Github and Google auth added

--- src/features/auth/components/register-form.tsx
+++ src/features/auth/components/register-form.tsx
@@ -51,6 +51,38 @@ export function RegisterForm() {
     },
   });
 
+  const signInGithub = async () => {
+    await authClient.signIn.social(
+      {
+        provider: "github",
+      },
+      {
+        onSuccess: () => {
+          router.push("/");
+        },
+        onError: () => {
+          toast.error("Something went wrong");
+        },
+      }
+    );
+  };
+
+  const signInGoogle = async () => {
+    await authClient.signIn.social(
+      {
+        provider: "google",
+      },
+      {
+        onSuccess: () => {
+          router.push("/");
+        },
+        onError: () => {
+          toast.error("Something went wrong");
+        },
+      }
+    );
+  };
+
   const onSubmit = async (values: RegisterFormValues) => {
     await authClient.signUp.email(
       {
@@ -87,6 +119,7 @@ export function RegisterForm() {
               <div className="grid gap-6">
                 <div className="flex flex-col gap-4">
                   <Button
+                    onClick={signInGithub}
                     variant={"outline"}
                     className="w-full"
                     type="button"
@@ -101,6 +134,7 @@ export function RegisterForm() {
                   </Button>
 
                   <Button
+                    onClick={signInGoogle}
                     variant={"outline"}
                     className="w-full"
                     type="button"
