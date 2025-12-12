311088a0f24a0be805616b1e61bb92e5c16851d5
Mangloo69 <yadav69.swayam@gmail.com>
2025-12-12 21:25:21 +0545

    prisma config fix

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { defineConfig } from "prisma/config";
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -7,8 +7,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url:
-      process.env.DATABASE_URL ||
-      "postgresql://localhost:5432/placeholder",
+    url: env("DATABASE_URL"),
   },
 });
